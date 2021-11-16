ec007931b2dd5481b27fe9a98650c90374a4afc7
kaderabdul79 <kaderabdul7980@gmail.com>
2021-11-16 13:03:42 +0600

    Update Route v6 and change the element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
-
+import Service from './components/Service/Service';
+import NotFound from './components/NotFound/NotFound';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,Route
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/service" element={<Service />}></Route>
+          {/* <Route path="" element={<Home />}></Route> */}
+          <Route path="/*" element={<NotFound />}></Route>
+        </Routes>
+      
+      </Router>
     </div>
   );
 }
